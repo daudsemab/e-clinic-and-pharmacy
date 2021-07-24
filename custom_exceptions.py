dd0c9770cc0bc625a22d7ea6d5f224b83d5e0e28
[Daud Semab] <[daudsemab.dxb@gmail.com]>
2021-07-24 18:00:48 +0500

    Seperated all custom exceptions in a new file custom_exceptions.py.

---
+++ custom_exceptions.py
@@ -0,0 +1,24 @@
+# Generic exception
+class Error(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class InvalidYear(Error):
+    pass
+
+
+class InvalidMonth(Error):
+    pass
+
+
+class InvalidDay(Error):
+    pass
+
+
+class InvalidName(Error):
+    pass
+
+
+class InvalidDates(Error):
+    pass
